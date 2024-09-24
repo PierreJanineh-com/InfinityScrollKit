0f437aafd078b9eb6d49b75e31cafc308e0e27f1
Pierre Janineh <janinehpierre@gmail.com>
2024-09-24 12:49:04 +0300

    public access

--- Sources/InfinityScrollKit/Options.swift
+++ Sources/InfinityScrollKit/Options.swift
@@ -11,18 +11,25 @@ import SwiftUI
 public struct Options<T> {
     
     /// The scroll's orientation
-    let orientation: Axis.Set = .vertical
+    let orientation: Axis.Set
     /// Count of items to display per page
-    let countPerPage: Int = 5
+    let countPerPage: Int
     /// Pagination customizations and advanced options
-    let paginationOptions: PaginationOptions? = nil
+    let paginationOptions: PaginationOptions?
     
-    /// Instance for customizing scroll's pagination
-    public struct PaginationOptions {
-        /// Callback for indicating when the scroll is loading new items
-        let onPageLoad: (() async -> [T])? = nil
-        /// Concatenation mode for loading items (comes along with `onPageLoad`
-        let concatMode: ConcatMode = .manual
+    /**
+     Options initializer for customizing count of items per page.
+     - Parameters:
+        - orientation: The orientation of the scroll.
+        - countPerPage: Number of items in a single page (default value is 5).
+        - paginationOptions: An instance for advanced pagination options.
+     */
+    public init(orientation: Axis.Set = .vertical,
+                countPerPage: Int = 5,
+                paginationOptions: PaginationOptions? = nil) {
+        self.orientation = orientation
+        self.countPerPage = countPerPage
+        self.paginationOptions = paginationOptions
     }
 }
 
@@ -34,5 +41,25 @@ extension Options {
         case auto
         /// Receive a new array containing all items (including new fetched items)
         case manual
+    }
+}
+
+extension Options {
+    
+    /// Instance for customizing scroll's pagination
+    public struct PaginationOptions {
+        /// Callback for indicating when the scroll is loading new items
+        let onPageLoad: (() async -> [T])?
+        /// Concatenation mode for loading items (comes along with `onPageLoad`
+        let concatMode: ConcatMode
+        
+        /**
+         Creates an instance to customize scroll pagination
+         */
+        public init(onPageLoad: (() async -> [T])? = nil,
+             concatMode: ConcatMode = .manual) {
+            self.onPageLoad = onPageLoad
+            self.concatMode = concatMode
+        }
     }
 }
